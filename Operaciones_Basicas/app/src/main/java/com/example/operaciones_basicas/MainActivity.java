be48ee2a03a3a9f8283ead6a91d41a2e2827aa94
Ricardobran <vladibran21@gmail.com>
2022-02-04 22:27:21 -0600

    GUIA1 DSM

---
+++ Operaciones_Basicas/app/src/main/java/com/example/operaciones_basicas/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.operaciones_basicas;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        tv1=findViewById(R.id.tv1);
+    }
+    //Este método se ejecutará cuando se presione el botón
+    public void sumar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1+nro2;
+        String resu=String.valueOf(suma);
+        tv1.setText(resu);
+    }
+
+    public void restar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int restar=nro1-nro2;
+        String resu=String.valueOf(restar);
+        tv1.setText(resu);
+    }
+
+    public void multiplicar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int multiplicar=nro1*nro2;
+        String resu=String.valueOf( multiplicar);
+        tv1.setText(resu);
+    }
+
+    public void dividir(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int dividir=nro1/nro2;
+        String resu=String.valueOf( dividir);
+        tv1.setText(resu);
+    }
+
+}
---
+++ java/src/ejercicio1/ejercicio4.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+public class ejercicio4 {
+	public static void main(String[] args) {
+		if (5 % 2 == 0) {
+			System.out.println("5 es divisible entre 2");
+		} else {
+			System.out.println("5 NO es divisible entre 2");
+		}
+}
+}
